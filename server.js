f051d8bafb976b0c99eb1293ffc99631b1114f57
theMagicMiner <johnnybellino@gmail.com>
2023-04-08 12:58:16 -0400

    a commit

--- server.js
+++ server.js
@@ -1,18 +1,14 @@
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
-}
+require('./environment')
 
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 
-const indexRouter = require('./routes/index')
-
 app.set("view engine", "ejs")
 app.set("views", __dirname + '/views')
 app.set('layout',  'layouts/layout')
 app.use(expressLayouts)
-app.use(express.static("public"))
+app.use(express.static(__dirname + "/public"))
 
 const mongoose = require('mongoose')
 
@@ -21,6 +17,10 @@ const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log("Connected to mongoose"))
 
+const indexRouter = require('./routes/index')
+const funnelRouter = require('./routes/funnel')
+
 app.use('/', indexRouter)
+app.use('/funnel/', funnelRouter)
 
 app.listen(process.env.PORT || 3000)
